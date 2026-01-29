1729f4450a223f815015454e48385094d9c49fdb
ziad <ziadmogadam@gmail.com>
2026-01-29 13:39:26 +0200

    category dynamic names

--- js/category.js
+++ js/category.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelectorAll(".nav-link");
 
     links.forEach(link => link.classList.remove("active-link"));
-
+    const categoryTitle = document.getElementById('categoryTitle');
     // Home
     if (path.includes("index.html") || path === "/") {
         document.querySelector('a[href="../index.html"]')?.classList.add("active-link");
@@ -16,15 +16,19 @@ document.addEventListener("DOMContentLoaded", () => {
         const cat = params.get("cat");
 
         if (cat === "sheet") {
+            categoryTitle.textContent = "Sheet Masks";
             document
                 .querySelector('a[href="category.html?cat=sheet"]')
                 ?.classList.add("active-link");
+                category
         } else if (cat === "stickers") {
+             categoryTitle.textContent = "Stickers";
             document
                 .querySelector('a[href="category.html?cat=stickers"]')
                 ?.classList.add("active-link");
         }
         else if (cat === "mugs") {
+            categoryTitle.textContent = "Mugs";
             document
                 .querySelector('a[href="category.html?cat=mugs"]')
                 ?.classList.add("active-link");
